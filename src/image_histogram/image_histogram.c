42d590e05d2a306db7058f18da93bdcfad7f29f7
Charlie <charlie@deepfileinfo.com>
2020-04-15 18:31:28 +0200

    Add binning and normalisation to histogram

--- src/image_histogram/image_histogram.c
+++ src/image_histogram/image_histogram.c
@@ -10,9 +10,33 @@ void count_hist(const unsigned char *img, int width, int height, int *hist) {
     }
 }
 
-void print_hist(const int *hist) {
-    for (int i = 0; i < 256; i++)
-        printf("%d, ", hist[i]);
+float *count_bins(const int *hist) {
+    float *bins = calloc(64, sizeof(float));
+
+    for (int i = 0; i < 256; i += 4) {
+        for (int j = i; j < i + 4; j++) {
+            bins[i / 4] += (float) hist[j];
+        }
+    }
+
+    return bins;
+}
+
+void normalise_bins(float *bins) {
+    float max = 0;
+    for (int i = 0; i < 64; i++) {
+        if (bins[i] > max)
+            max = bins[i];
+    }
+
+    for (int i = 0; i < 64; i++) {
+        bins[i] = bins[i] / max;
+    }
+}
+
+void print_hist(const float *bins) {
+    for (int i = 0; i < 64; i++)
+        printf("%f, ", bins[i]);
 }
 
 int main(int argc, char **argv) {
@@ -31,7 +55,13 @@ int main(int argc, char **argv) {
     int hist[256] = {0};
     count_hist(img, width, height, hist);
 
-    print_hist(hist);
+    float *bins = count_bins(hist);
+
+    normalise_bins(bins);
+
+    print_hist(bins);
+
+    free(bins);
 
     return 0;
 }
