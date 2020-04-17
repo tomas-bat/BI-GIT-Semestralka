98af1f34f690a97a4806e43bd7b4e1adb24a5435
Charlie <charlie@deepfileinfo.com>
2020-04-17 11:21:48 +0200

    histogram, Add documentation

--- src/image_histogram/image_histogram.c
+++ src/image_histogram/image_histogram.c
@@ -8,6 +8,8 @@
 
 #define HIST_HEIGHT 10
 
+/// Get width of terminal as a power of 2 (for easy binning) up to 256.
+/// \return Terminal width or 64 if fails.
 int get_hist_width() {
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -21,12 +23,21 @@ int get_hist_width() {
     return hist_width;
 }
 
+/// Count 8bpp histogram of the image.
+/// \param img          Image array.
+/// \param width        Width of the image.
+/// \param height       Height of the image.
+/// \param hist[out]    Pointer to array where histrogram is accumulated.
 void count_hist(const unsigned char *img, int width, int height, int *hist) {
     for (int i = 0; i < width * height; i++) {
         hist[img[i]]++;
     }
 }
 
+/// Group histogram to bins.
+/// \param hist         Original histogram.
+/// \param bin_count    Number of output bins
+/// \return             Newly allocated array with absolute number of pixels in each bin.
 float *count_bins(const int *hist, int bin_count) {
     float *bins = calloc(bin_count, sizeof(float));
 
@@ -40,6 +51,9 @@ float *count_bins(const int *hist, int bin_count) {
     return bins;
 }
 
+/// Normalize bins such that the maximum value is HIST_HEIGHT
+/// \param bins[in,out] Bins to be normalised in place.
+/// \param bin_count    Bin count.
 void normalise_bins(float *bins, int bin_count) {
     float max = 0;
     for (int i = 0; i < bin_count; i++) {
@@ -52,6 +66,9 @@ void normalise_bins(float *bins, int bin_count) {
     }
 }
 
+/// Print the histogram.
+/// \param bins     Bins to be printed.
+/// \param width    Number of bins.
 void print_hist(const float *bins, int width) {
     char *boxes[16] = {" ", "▁", "▁", "▂", "▂", "▃", "▃", "▄", "▄", "▅", "▅", "▆", "▆", "▇", "▇", "█"};
 
